633842fbf5c782b4fbb9334f0c9c698ea41e46ea
Luke Tutton <tuttonluke@gmail.com>
2022-12-19 11:35:34 +0000

    Adds download_images method.

--- project_files/prepare_image_data.py
+++ project_files/prepare_image_data.py
@@ -1,6 +1,7 @@
 #%%
 import boto3
 import pandas as pd
+import os
 #%%
 class ImageProcessing:
     def __init__(self) -> None:
@@ -18,7 +19,23 @@ class ImageProcessing:
             self.list_of_folders.append(prefix["Prefix"][:-1])
 
     def download_images(self):
-        pass
+        """Downloads images from s3 bucket, all images from a single
+        property being saved into a folder named with the property ID.
+        """
+        for element in self.list_of_folders[:2]:
+            os.mkdir(f"images/{element}")
+            list_of_files = []
+            response = self.client.list_objects_v2(Bucket=self.bucket_name,
+                                                    Prefix=f"{element}")
+            objects = response["Contents"]
+            for object in objects:
+                list_of_files.append(object["Key"])
+            for file in list_of_files:
+                self.client.download_file(
+                    self.bucket_name,
+                    f"{file}",
+                    f"images/{file}"
+                )
 
     def resize_images(self):
         pass
@@ -26,6 +43,4 @@ class ImageProcessing:
 if __name__ == "__main__":
     image_processor = ImageProcessing()
     image_processor.get_list_of_folders()
-
-    print(image_processor.list_of_folders)                                      
-#%%
+    image_processor.download_images()                                  
