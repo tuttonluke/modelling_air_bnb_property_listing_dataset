57687954eb47d98afbdc531932275684c72a0e05
Luke Tutton <tuttonluke@gmail.com>
2023-01-09 09:44:31 +0000

    Adds random seed and changes model to SGDRegressor.

--- project_files/modelling.py
+++ project_files/modelling.py
@@ -1,12 +1,15 @@
 #%%
 from read_tabular_data import TabularData
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import SGDRegressor
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
 #%%
 if __name__ == "__main__":
     # load in data
+    np.random.seed(42)
     tabular_df = TabularData()
     numerical_tabular_df = tabular_df.get_numerical_data_df()
 
@@ -27,12 +30,15 @@ if __name__ == "__main__":
                                                                 test_size=0.3
                                                                 )
 
-    model = LinearRegression()
+    model = SGDRegressor()
     model.fit(X_train, y_train)
     
     y_train_pred = model.predict(X_train)
     y_validation_pred = model.predict(X_validation)
     y_test_pred = model.predict(X_test)
+
+    mse_loss = mean_squared_error(y_validation, y_validation_pred)
+    print("Mean Squared Error Loss on Validation data: ", mse_loss)
 
     plt.figure()
     plt.scatter(np.arange(50), y_validation_pred[:50], c="r", label="Predictions")
