639bb10fb4c79a4dece4a5598cd7688151eef33b
Luke Tutton <tuttonluke@gmail.com>
2023-01-31 17:28:16 +0000

    Imports numerical dataset.

--- project_files/pytorch_models.py
+++ project_files/pytorch_models.py
@@ -1,4 +1,6 @@
 # %%
+from read_tabular_data import TabularData
+from regression_modelling import read_in_data
 from torch.utils.data import Dataset
 import torch
 # %%
@@ -14,4 +16,13 @@ class AirbnbNightlyPriceImageDataset(Dataset):
     
     def __len__(self):
         return len(self.features)
+# %%
+if __name__ == "__main__":
+    tabular_df = TabularData()
+    numerical_tabular_df = tabular_df.get_numerical_data_df()
+
+    feature_df_scaled, label_series = read_in_data()
+
+    dataset = AirbnbNightlyPriceImageDataset(feature_df_scaled, label_series)
+
 # %%
