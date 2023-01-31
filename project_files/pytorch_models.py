6650f320bd40dd8cdb2487a8c764e8273ab45a0e
Luke Tutton <tuttonluke@gmail.com>
2023-01-31 19:43:06 +0000

    Adds DataLoader.

--- project_files/pytorch_models.py
+++ project_files/pytorch_models.py
@@ -1,10 +1,14 @@
 # %%
 from read_tabular_data import TabularData
 from regression_modelling import read_in_data
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import random_split
 import torch
 # %%
 class AirbnbNightlyPriceImageDataset(Dataset):
+    """Creates a PyTorch dataset  of the AirBnb data that returns a tuple 
+    of (features, labels) when indexed.
+    """
     def __init__(self, features, labels) -> None:
         super().__init__()
         assert len(features) == len(labels) # ensure features and label sets are the same length
@@ -18,11 +22,23 @@ class AirbnbNightlyPriceImageDataset(Dataset):
         return len(self.features)
 # %%
 if __name__ == "__main__":
+    # import AirBnb dataset, isolate numerical data and split it into features and labels
     tabular_df = TabularData()
     numerical_tabular_df = tabular_df.get_numerical_data_df()
-
     feature_df_scaled, label_series = read_in_data()
-
+    
+    # create custom torch dataset with the imported data
     dataset = AirbnbNightlyPriceImageDataset(feature_df_scaled, label_series)
+
+    # split data into train, test, and validation sets
+    train_subset, test_subset = random_split(dataset, [663, 166])
+    test_subset, val_subset = random_split(test_subset, [132, 34])
+
+    # create DataLoader to load in data for each set
+    BATCH_SIZE = 10
+    train_loader = DataLoader(train_subset, shuffle=True, batch_size=BATCH_SIZE)
+    test_loader = DataLoader(test_subset, shuffle=True, batch_size=BATCH_SIZE)
+    val_loader = DataLoader(val_subset, shuffle=True, batch_size=BATCH_SIZE)
+
 
 # %%
