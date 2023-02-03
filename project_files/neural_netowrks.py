47c74e02dfbdd1f30574e40b7d3cc749aa6ed822
Luke Tutton <tuttonluke@gmail.com>
2023-02-03 16:44:41 +0000

    Adds AirBnBNightlyPriceImageDataset class.

--- project_files/neural_netowrks.py
+++ project_files/neural_netowrks.py
@@ -9,3 +9,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 import yaml
 # %%
+class AirBnBNightlyPriceImageDataset(Dataset):
+    """Creates a PyTorch dataset  of the AirBnb data that returns a tuple 
+    of (features, labels) when indexed.
+    """
+    def __init__(self, features, labels) -> None:
+        super().__init__()
+        # assert feature and label sets are the same length
+        assert len(features) == len(labels)
+        self.features = features
+        self.labels = labels
+    
+    def __getitem__(self, index):
+        return torch.tensor(self.features[index], dtype=torch.float32), torch.tensor(self.labels[index], dtype=torch.float32)
+
+    def __len__(self):
+        return len(self.features)
+
+
+        
