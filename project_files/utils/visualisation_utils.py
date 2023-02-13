e1db47662fdfe58d63f7ce82c144e765d18e3647
Luke Tutton <tuttonluke@gmail.com>
2023-02-13 14:50:51 +0000

    Adds confusion matrix visualisation function.

--- project_files/utils/visualisation_utils.py
+++ project_files/utils/visualisation_utils.py
@@ -1,6 +1,10 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, classification_report
+
 # %%
 def visualise_features_vs_target(X: np.ndarray, y: np.ndarray, feature_names: list, target: str):
     """Creates a 3x4 plot which visualises each feature seperately against
@@ -34,3 +38,22 @@ def visualise_features_vs_target(X: np.ndarray, y: np.ndarray, feature_names: li
         if i % 4 == 0:
             ax.set_ylabel(f"{target}", size=14)
     plt.show()
+
+def visualise_classification_metrics(accuracy_stats, loss_stats, y_test_pred, y_test_true):
+    # test visualisations
+    # Create dataframes
+    train_val_acc_df = pd.DataFrame.from_dict(accuracy_stats).reset_index().melt(id_vars=['index']).rename(columns={"index":"epochs"})
+    train_val_loss_df = pd.DataFrame.from_dict(loss_stats).reset_index().melt(id_vars=['index']).rename(columns={"index":"epochs"})
+    # Plot the dataframes
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(20,7))
+    sns.lineplot(data=train_val_acc_df, x = "epochs", y="value", hue="variable",  ax=axes[0]).set_title('Train-Val Accuracy/Epoch')
+    sns.lineplot(data=train_val_loss_df, x = "epochs", y="value", hue="variable", ax=axes[1]).set_title('Train-Val Loss/Epoch')
+
+
+    confusion_matrix_df = pd.DataFrame(confusion_matrix(y_test_true, y_test_pred))
+    fig2, ax2 = plt.subplots(nrows=1, ncols=1)
+    sns.heatmap(confusion_matrix_df, annot=True)
+
+    print(classification_report(y_test_true, y_test_pred))
+
+    return fig, fig2
